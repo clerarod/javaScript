797195a697f6accdfda5885ba81da9062bcbc7d0
Clera Rodrigues <clerarod2003@gmail.com>
2025-01-16 19:48:15 +0530

    first script

---
+++ script.js
@@ -0,0 +1,10 @@
+
+alert=" java is amazing";
+
+
+
+const firstName = 'Jonas';
+const age = '21';
+const job = 'Teacher';
+
+console.log(`My name is ${firstName} , i'm ${age} year old. i'm a ${job}.`)
