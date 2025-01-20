0e3c03bdaad7a36adf53223801d37babc5adcdf6
Clera Rodrigues <clerarod2003@gmail.com>
2025-01-20 20:45:51 +0530

    js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
-alert=" java is amazing";
 
 
 
-const firstName = 'Jonas';
-const age = '21';
-const job = 'Teacher';
+// const firstName = 'Jonas';
+// const age = '21';
+// const job = 'Teacher';
 
-console.log(`My name is ${firstName} , i'm ${age} year old. i'm a ${job}.`)
+// console.log(`My name is ${firstName} , i'm ${age} year old. i'm a ${job}.`)
+
+// const  inputYear = '1991';
+// console.log(inputYear + 18);
+
+
+const inputYear = '1991';
+console.log(inputYear + 18);
+
